88f20a0e7c52d2ea581616d85608cc8d634c6ad1
Km-Nazrul-Islam <nazrulislamrongon@gmail.com>
2023-06-10 16:15:27 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,11 @@ const app: Application = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extended: True }))
+app.use(express.urlencoded({ extended: true }))
 
 //testing perpas
 app.get('/', (req: Request, res: any) => {
   res.send('Working Successfully')
 })
 
-export default app
+export default apps
