fabf01bb285e7296ebb4660410afbe0889b681f2
Km-Nazrul-Islam <nazrulislamrongon@gmail.com>
2023-06-10 22:32:15 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }))
 
 //testing perpas
 app.get('/', (req: Request, res: any) => {
-  res.send('Working Successfully')
+  res.send('Working Successfully true')
 })
 
 export default app
